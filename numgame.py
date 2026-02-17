94f39c518bfd58314e103565b1357b140f748caf
Azizbek Topilboyev <azizbektopilboyev7@gmail.com>
2026-02-17 22:10:49 +0500

    will be silent

--- numgame.py
+++ numgame.py
@@ -1,14 +1,13 @@
-import random 
+import random
 
-guess=int(random.randint()*10)
-n=int(input('son kirit'))
-guesses=0
+guess = int(random.randint() * 10)
+n = int(input("son kirit"))
+guesses = 0
 while True:
-		if n==guess:
-			print('topding')
-			break
-		guesses+=1
+    if n == guess:
+        print("topding")
+        break
+    guesses += 1
 
-		if guess>=3:
-			print('yutqading')
-		
+    if guess >= 3:
+        print("yutqading")
